ba7b8ce66a14b64922372d45b1500c12cc4ca0c9
sakir2141 <117640302+sakir2141@users.noreply.github.com>
2023-01-05 18:10:46 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,24 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "var.region"
+  access_key = "var.access_key"
+  secret_key = "var.secret_key"
+}
+
+resource "aws_instance" "s1" {
+  ami           = "ami-0cca134ec43cf708f"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-jenkins"
+  }
+}
